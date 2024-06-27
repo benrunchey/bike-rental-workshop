0f04486016b761df7170598169a5b60dcc1e1ffd
Ben Runchey <Ben Runchey>
2024-06-27 13:56:46 -0500

    Adding RetryScheduler to CommandGateway to handle retrying commands that fail due to non-transient exceptions (a.k.a - Business Exceptions)

--- rental/src/main/java/io/axoniq/demo/bikerental/RentalApplication.java
+++ rental/src/main/java/io/axoniq/demo/bikerental/RentalApplication.java
@@ -3,6 +3,10 @@ package io.axoniq.demo.bikerental;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.axoniq.demo.bikerental.coreapi.payment.PaymentStatus;
 import io.axoniq.demo.bikerental.coreapi.rental.BikeStatus;
+import org.axonframework.commandhandling.CommandBus;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.commandhandling.gateway.DefaultCommandGateway;
+import org.axonframework.commandhandling.gateway.IntervalRetryScheduler;
 import org.axonframework.config.Configuration;
 import org.axonframework.config.EventProcessingConfigurer;
 import org.axonframework.deadline.DeadlineManager;
@@ -20,12 +24,24 @@ import org.springframework.context.annotation.Bean;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
-@EntityScan(basePackageClasses = {PaymentStatus.class, BikeStatus.class, DomainEventEntry.class, SagaEntry.class, TokenEntry.class})
+//@EntityScan(basePackageClasses = {PaymentStatus.class, BikeStatus.class, DomainEventEntry.class, SagaEntry.class, TokenEntry.class})
 @SpringBootApplication
 public class RentalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RentalApplication.class, args);
+    }
+
+
+    //configuring retry scheduler for command gateway.  This will handle retrying if a non-transient exception occurs
+    //when attempting to send a command
+    //https://docs.axoniq.io/reference-guide/axon-framework/axon-framework-commands/infrastructure#retryscheduler
+    @Bean
+    public CommandGateway commandGatewayWithRetry(CommandBus commandBus){
+
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(5);
+        IntervalRetryScheduler rs = IntervalRetryScheduler.builder().retryExecutor(scheduledExecutorService).maxRetryCount(5).retryInterval(1000).build();
+        return DefaultCommandGateway.builder().commandBus(commandBus).retryScheduler(rs).build();
     }
 
     @Bean(destroyMethod = "shutdown")
@@ -38,6 +54,9 @@ public class RentalApplication {
         objectMapper.activateDefaultTyping(objectMapper.getPolymorphicTypeValidator(),
                                            ObjectMapper.DefaultTyping.JAVA_LANG_OBJECT);
     }
+
+
+
 
     @Autowired
     public void configure(EventProcessingConfigurer eventProcessing) {
